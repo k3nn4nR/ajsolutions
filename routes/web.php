6a317dc3237350c088c7425e185b8eee64265cb7
kennan <kennan_11@hotmail.com>
2021-09-18 21:20:54 -0500

    configuracion inicial, y avance de plantillas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/role', 'RoleController@listar');
+Route::post('role', 'RoleController@store');
+Route::put('role', 'RoleController@update');
